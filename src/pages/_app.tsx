7edbfa2d1539fb7a6c623feca63b1283ee91a28a
Anatolii <49586997+TolyaDeveloper@users.noreply.github.com>
2022-07-12 16:53:04 +0300

    init application (#1)

---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import 'styles/globals.scss'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>Freshness ecommerce</title>
+        <meta name="title" content="Freshness ecommerce" />
+        <meta
+          name="description"
+          content="Freshness ecommerce - your favorite food"
+        />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        {/* <link rel="icon" href="/icon.svg" type="image/svg+xml" /> */}
+        {/* <link rel="apple-touch-icon" href="/apple-touch-icon.png" /> 180x180 */}
+        {/* <link rel="manifest" href="/manifest.webmanifest" /> */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
