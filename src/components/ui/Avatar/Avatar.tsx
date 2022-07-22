8f15b05b8c00a0f066531cff31ff7a744cbda358
Anatolii <kabantolyan@gmail.com>
2022-07-22 11:26:05 +0300

    add avatar component

---
+++ src/components/ui/Avatar/Avatar.tsx
@@ -0,0 +1,28 @@
+import { cnb } from 'cnbuilder'
+import { AvatarProps } from './Avatar.props'
+import Image from 'next/image'
+
+import styles from './Avatar.module.scss'
+
+const Avatar = ({
+  className,
+  width = 48,
+  height = 48,
+  src,
+  alt,
+  ...props
+}: AvatarProps) => {
+  return (
+    <Image
+      className={cnb(styles.avatar, className)}
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      objectFit="cover"
+      {...props}
+    />
+  )
+}
+
+export default Avatar
